782d1def1584bc5cb69ff7afe88d41f524f60a39
liang liu <liangliu@thoughtworks.com>
2014-09-29 16:17:45 +0800

    init the repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HomeworkRor::Application.config.secret_key_base = '6b779b39814a29599c9882f0ec4c50cf2b9e6d38187114e2cbb2af6916bcd43db09a772dd2035804fda8fcf7d12b9f307557aaa1a393691e6308f2d88eeb23f9'
